c8abf520713d4e22b1dcb82f17ceb5fcd87d144d
Jenny12120 <wjenny201697@gmail.com>
2025-11-29 02:10:59 -0500

    remove items not used

--- Server/database_service.py
+++ Server/database_service.py
@@ -1,8 +1,4 @@
 import pymysql.cursors
-import redis
-
-import ssl
-import os
 
 MYSQL_HOST = "anime-convention-tracker-animeconventiontracker2025.h.aivencloud.com" 
 MYSQL_PORT = 26181
@@ -24,19 +20,3 @@ def connect_to_db():
                 cursorclass=pymysql.cursors.DictCursor
             )
     return connection
-
-def connect_to_redis():
-    r = redis.Redis(
-        host='redis-17948.crce174.ca-central-1-1.ec2.redns.redis-cloud.com',
-        port=17948,
-        password='WIgd8aLQhGYg35oJJhovyD0pK8Sf7kGK',
-        decode_responses=True)
-    return r
-
-def initialize_convention_counter():
-    r = connect_to_redis()    
-    r.setnx('global:convention_id_counter', 0)
-    
-def get_next_convention_id(r: redis.Redis):
-    new_id = r.incr('global:convention_id_counter')
-    return new_id
